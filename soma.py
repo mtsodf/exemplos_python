4f37ef3c8407ea1676b030b61b225e89eba42020
Mateus Oliveira <mtsodf@gmail.com>
2024-07-29 18:02:27 -0300

    feat: Adicionar código para somar duas variáveis

--- soma.py
+++ soma.py
@@ -1,8 +1,14 @@
 # Esse programa cria duas variáveis a e b, atribui valores a elas e depois soma esses valores.
 # O valor da soma é impresso na tela.
+
+# Atribui o valor 10 à variável 'a'
 a = 10
+
+# Atribui o valor 5 à variável 'b'
 b = 5
 
+# Soma os valores das variáveis 'a' e 'b' e armazena o resultado na variável 'soma'
 soma = a + b
 
+# Imprime o valor da soma na tela
 print("O valor da soma é", soma)
