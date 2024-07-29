d6c55ffbec1656fb0d1ce2e7f8ef9c1be0097b09
Mateus Oliveira <mtsodf@gmail.com>
2024-07-29 17:59:25 -0300

    Adicionar código para exibir o nome digitado pelo usuário

---
+++ exemplo_input.py
@@ -0,0 +1,11 @@
+# Exibe uma mensagem pedindo ao usuário para digitar seu nome
+print("Digite seu nome e aperte enter:")
+
+# Recebe a entrada do usuário e armazena na variável 'nome'
+nome = input()
+
+# Cria uma mensagem concatenando a string "Meu nome é " com o valor da variável 'nome'
+mensagem = "Meu nome é " + nome
+
+# Exibe a mensagem final
+print(mensagem)
