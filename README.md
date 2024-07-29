5efcc2ac7756fc0305e853cff37ea197d380c6b6
Mateus Oliveira <mtsodf@gmail.com>
2024-07-29 16:26:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Objetivo
+
+Esse repositório tem como intuito guardar códigos python com exemplos simples para quem está começando a aprender.
+Cada arquivo apresenta um exemplo de código com um objetivo específico, e a descrição do que o código faz.
---
+++ print_varias_linhas.py
@@ -0,0 +1,24 @@
+# Esse programa imprime um texto em várias linhas de 3 maneiras diferentes.
+
+# Utilizando vários métodos prints
+print("Método 1")
+
+print("Um dia, um cão ia atravessando uma ponte, carregando um osso na boca.")
+print("Olhando para baixo, viu sua própria imagem refletida na água.")
+print("Pensando ver outro cão, cobiçou-lhe logo o osso e pôs-se a latir.")
+print("Mal, porém, abriu a boca, seu próprio osso caiu na água e se perdeu para sempre.")
+
+# Utilizando apenas o \n e apenas um print
+print("\nMétodo 2")
+print(
+    "Um dia, um cão ia atravessando uma ponte, carregando um osso na boca.\nOlhando para baixo, viu sua própria imagem refletida na água.\nPensando ver outro cão, cobiçou-lhe logo o osso e pôs-se a latir.\nMal, porém, abriu a boca, seu próprio osso caiu na água e se perdeu para sempre."
+)
+
+# Utilizando  """ para definir uma instrução em várias
+print("\nMétodo 3")
+print(
+    """Um dia, um cão ia atravessando uma ponte, carregando um osso na boca.
+Olhando para baixo, viu sua própria imagem refletida na água.
+Pensando ver outro cão, cobiçou-lhe logo o osso e pôs-se a latir.
+Mal, porém, abriu a boca, seu próprio osso caiu na água e se perdeu para sempre."""
+)
