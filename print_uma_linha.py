19a2d2952ed8d083033586446b73185c4b6f18e3
Mateus Oliveira <mtsodf@gmail.com>
2024-07-29 17:46:11 -0300

    feat: Adicionar código para imprimir uma linha com o nome Mateus

---
+++ print_uma_linha.py
@@ -0,0 +1,15 @@
+# Esse código faz o print de uma linha só
+print("O meu nome é Mateus")
+
+# Fazendo a mesma coisa que o código acima mas colocando o nome em uma variável
+
+# Essa linha é uma atribuição de valor "Mateus" na variável nome
+nome = "Mateus"
+
+# Essa linha faz o print da variável nome
+print(nome)
+
+# Essa linha faz o print da variável nome concatenada com a string "O meu nome é "
+# O símbolo de + é o operador de concatenação de strings
+# Concatenação é a operação de juntar duas strings
+print("O meu nome é " + nome)
