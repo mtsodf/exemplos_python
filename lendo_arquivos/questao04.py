37684e0651a3113dfb75e9c6c6ba314077de2d11
Mateus Oliveira <mtsodf@gmail.com>
2024-10-30 21:51:46 -0300

    Solução da questão04.py

---
+++ lendo_arquivos/questao04.py
@@ -0,0 +1,33 @@
+"""
+Questão 4: Filmes/Séries Lançados antes de 2010
+	Enunciado: Liste os dez melhores filmes lançados até 2010.
+"""
+
+from questao01 import read_csv
+
+
+cabecalho, dados = read_csv("./imdb_data.csv")
+
+coluna_ano = cabecalho.index("releaseYear")
+coluna_tipo = cabecalho.index("type")
+coluna_titulo = cabecalho.index("title")
+coluna_nota = cabecalho.index("averageRating")
+
+filmes_selecionados = []
+
+for dado in dados:
+    ano = int(dado[coluna_ano])
+    tipo = dado[coluna_tipo]
+    titulo = dado[coluna_titulo]
+    nota = float(dado[coluna_nota])
+
+    if ano < 2010 and tipo == "movie":
+        filmes_selecionados.append((nota, ano, titulo))
+
+
+filmes_selecionados.sort()
+
+filmes_selecionados = filmes_selecionados[::-1]
+
+for filme in filmes_selecionados[:10]:
+    print(filme)
